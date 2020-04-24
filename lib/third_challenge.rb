70c44669b585874374c82eae560ef8518366f160
Kevin C Malin <racmalin@yahoo.com>
2020-04-24 04:03:07 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -4,11 +4,11 @@ def third_challenge
       :patriarch => {:name => "Lord Montague", :age => "53"
         
         },
-      :matriarch => {
+      :matriarch => {:name => "Lady Montague", :age => "54"
         
         },
-      :hero => {
-        #your key/value pairs here
+      :hero => {:name => "Romeo", :age => "15"
+        
         },
       :hero_friends => []
    }, 
