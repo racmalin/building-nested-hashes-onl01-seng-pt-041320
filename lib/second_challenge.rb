e3c770988a4682caded0d6e66d114d38509873f5
Kevin C Malin <racmalin@yahoo.com>
2020-04-24 04:32:33 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,10 +1,17 @@
 def second_challenge
   epic_tragedy = {
-    :montague => {:patriarc => {}, :matriarc => {}, :hero => {}, :hero_friends => []
-      }, 
-    :capulet => {:patriarc => {}, :matriarc => {}, :heroine => {}, :heroine_friends => []
-      
-      }}
-
+   :montague => {
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
+   },
+   :capulet => {
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
+   }
+}
   
 end
