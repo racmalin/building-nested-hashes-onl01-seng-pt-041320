09151418c6690250b32296cca2b0067adfed73c2
Kevin C Malin <racmalin@yahoo.com>
2020-04-24 04:01:23 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,11 +1,11 @@
 def third_challenge
   epic_tragedy = {
    :montague => {
-      :patriarch => {:name =>  "Lord Montague"
+      :patriarch => {:name => "Lord Montague", :age => "53"
         
         },
       :matriarch => {
-        #your key/value pairs here
+        
         },
       :hero => {
         #your key/value pairs here
