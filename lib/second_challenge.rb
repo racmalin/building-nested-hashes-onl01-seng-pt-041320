2a555ed1af21c8df617fa656f9e294db535935ed
Kevin C Malin <racmalin@yahoo.com>
2020-04-24 03:59:20 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,7 +2,7 @@ def second_challenge
   epic_tragedy = {
     :montague => {:patriarc => {}, :matriarc => {}, :hero => {}, :hero_friends => []
       }, 
-    :capulet => {
+    :capulet => {:patriarc => {}, :matriarc => {}, :heroine => {}, :heroine_friends => []
       
       }}
 
